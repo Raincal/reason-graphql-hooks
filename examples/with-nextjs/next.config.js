5a4e6cba98e41e99888886339eba9285d489cad8
Raincal <cyj94228@gmail.com>
2020-02-11 19:01:28 +0800

    fix: compatible with next-transpile-modules 3.x

--- examples/with-nextjs/next.config.js
+++ examples/with-nextjs/next.config.js
@@ -1,10 +1,10 @@
 const bsconfig = require('./bsconfig.json');
 const withCSS = require('@zeit/next-css');
-const withTM = require("next-transpile-modules");
+const withTM = require('next-transpile-modules')(['bs-platform'].concat(bsconfig['bs-dependencies']));
+
 const config = {
   target: 'serverless',
-  pageExtensions: ["jsx", "js", "bs.js"],
-  transpileModules: ["bs-platform"].concat(bsconfig["bs-dependencies"])
+  pageExtensions: ['jsx', 'js', 'bs.js']
 };
 
 module.exports = withTM(withCSS(config));
